4cbb34e2a9d3dd97d24152719a87b3299f0fefd1
Jacek Witkowski <jacek.witkowski@cern.ch>
2018-12-08 14:54:57 +0100

    Added some auto-generated seed project.

---
+++ src/main/java/com/jwsolutions/pretiustest/PretiusTestApplication.java
@@ -0,0 +1,12 @@
+package com.jwsolutions.pretiustest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PretiusTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PretiusTestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jwsolutions/pretiustest/PretiusTestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.jwsolutions.pretiustest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PretiusTestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
